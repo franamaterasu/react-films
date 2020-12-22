c8e1d348d8ff8d41009c27135cf737d5f17d195e
Francisco Villar Salvador <franvlive@gmail.com>
2020-12-22 19:14:20 +0100

    Creación de la vista individual de las películas

--- src/components/film/film.js
+++ src/components/film/film.js
@@ -16,20 +16,46 @@ const Film = () => {
       .catch((e) => console.error(e));
   }, []);
 
-  const { backdrop_path, title, overview } = film;
+  const {
+    backdrop_path,
+    title,
+    overview,
+    genres,
+    vote_average,
+    vote_count,
+  } = film;
 
   const bannerBg = `https://image.tmdb.org/t/p/original${backdrop_path}`;
 
   return (
-    <>
+    <article>
       <section
         className="film-banner"
         style={{ backgroundImage: `url('${bannerBg}')` }}
       >
-        <p>{title}</p>
-        <p>{overview}</p>
+        <div className="film-banner__info">
+          <p className="film-banner__title">{title}</p>
+          <p className="film-banner__overview">{overview}</p>
+          <div className="film-banner__details">
+            {/* 
+            <div className="film-banner__genres">
+              {genres.map((genre) => (
+                <article className="film-banner__genre">{genre.name}</article>
+              ))}
+            </div>
+            */}
+            <div className="film-banner__votes">
+              <article className="film-banner__votes-item">
+                Número de votos: {vote_count}
+              </article>
+              <article className="film-banner__votes-item" title="Nota media">
+                Nota media: {vote_average}
+              </article>
+            </div>
+          </div>
+        </div>
       </section>
-    </>
+    </article>
   );
 };
 
