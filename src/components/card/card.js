0276adb570b4c3e4a0b681d04b5ba60bcbc87fec
Francisco Villar Salvador <franvlive@gmail.com>
2020-12-22 18:03:22 +0100

    Añadimos al componente Card el link a su propia película

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 import "./card.scss";
 
 const Card = (props) => {
-  const { title, poster_path, overview } = props.cardInfo;
+  const { id, title, poster_path, overview } = props.cardInfo;
 
   const posterUrl = `https://image.tmdb.org/t/p/w500/${poster_path}`;
 
   return (
     <article className="card">
-      <img className="card__image" src={posterUrl} alt={title} />
-      <section className="card__info">
-        <h4 className="card__title">{title}</h4>
-        <p className="card__resume">{overview}</p>
-      </section>
+      <Link to={`/movie/${id}`}>
+        <img className="card__image" src={posterUrl} alt={title} />
+        <section className="card__info">
+          <h4 className="card__title">{title}</h4>
+          <p className="card__resume">{overview}</p>
+        </section>
+      </Link>
     </article>
   );
 };
