197950d83439d001b9deb58042941aabc3544fad
Francisco Villar Salvador <franvlive@gmail.com>
2020-12-22 19:13:52 +0100

    Añadido el link para mostrar la película individual

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Header from "./components/header";
 import Grid from "./components/grid";
+import Film from "./components/film";
 import useSearch from "./components/useSearch";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const { films, searchByTitle } = useSearch();
 
   return (
     <>
-      <Header onSearch={searchByTitle} />
-      <Grid films={films} />
+      <Router>
+        <Header onSearch={searchByTitle} />
+        <Switch>
+          <Route path="/" exact={true}>
+            <Grid films={films} />
+          </Route>
+          <Route path="/movie/:id" exact={true}>
+            <Film />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
