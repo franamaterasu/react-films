17bdf05fc6010b333b35d201750f5022a0ed92c3
Francisco Villar Salvador <franvlive@gmail.com>
2020-12-22 16:10:32 +0100

    Creación del componente Grid

---
+++ src/components/grid/grid.js
@@ -0,0 +1,13 @@
+import Card from "../card";
+
+import "./grid.scss";
+
+const Grid = () => {
+  return (
+    <section className="grid">
+      <Card />
+    </section>
+  );
+};
+
+export default Grid;
