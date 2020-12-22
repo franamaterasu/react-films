cfeaf75abd81f71ace7c6f0d694fe16c9e7f913a
Francisco Villar Salvador <franvlive@gmail.com>
2020-12-22 16:17:24 +0100

    Nos conectamos a la API y pasamos el resultado por props al componente Grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import Header from "./components/header";
+import Grid from "./components/grid";
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=7cdd6813e009397c594758fe7bce7b47&language=en-US&page=1"
+    )
+      .then((res) => res.json())
+      .then((res) => setFilms(res.results))
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <>
       <Header />
+      <Grid films={films} />
     </>
   );
 }
