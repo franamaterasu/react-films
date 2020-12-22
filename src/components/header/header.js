854661442faf2c77af12b8ed658d091aa2100b29
Francisco Villar Salvador <franvlive@gmail.com>
2020-12-22 17:20:59 +0100

    Creamos el input del header con el que filtraremos las películas

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,24 @@
 import "./header.scss";
 
-const Header = () => {
+const Header = ({ onSearch }) => {
   return (
     <header className="header">
       <a className="header__logo" href="@">
         ReactFilms
       </a>
-      <nav className="header__nav">
-        <li className="header__nav-item">Home</li>
-        <li className="header__nav-item">Top</li>
-        <li className="header__nav-item">Recents</li>
-      </nav>
+      <section className="header__actions">
+        <nav className="header__nav">
+          <li className="header__nav-item">Home</li>
+          <li className="header__nav-item">Top</li>
+          <li className="header__nav-item">Recents</li>
+        </nav>
+        <input
+          onChange={onSearch}
+          className="header__search"
+          type="text"
+          placeholder="Busca cualquier película..."
+        />
+      </section>
     </header>
   );
 };
