9c322f811e09719dfcc5d82da1134f1d041e683a
Francisco Villar Salvador <franvlive@gmail.com>
2021-01-23 10:35:26 +0100

    Header - Añadir selección de tema

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Header from "./components/header";
 import Film from "./components/film";
 import Home from "./pages/home";
@@ -5,6 +6,7 @@ import Top from "./pages/top";
 import Recents from "./pages/recents";
 import useSearch from "./components/useSearch";
 import useSlider from "./components/useSlider";
+import ThemeContext from "./context/themeContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -24,38 +26,47 @@ function App() {
 
   const { slider } = useSlider();
 
+  const [theme, setTheme] = useState(true);
+
+  const changeTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
     <>
       <Router>
-        <Header
-          onSearch={searchByTitle}
-          onFocus={hideSlider}
-          onBlur={showSlider}
-          handleHomeClick={showHomeFilms}
-          handleTopClick={showTopFilms}
-          handleRecentClick={showRecentFilms}
-        />
-        <Switch>
-          <Route path="/" exact={true}>
-            <Home
-              films={films}
-              slider={slider}
-              page={page}
-              handleMoreClick={handleMoreClick}
-              handleLessClick={handleLessClick}
-              sliderHeight={sliderHeight}
-            />
-          </Route>
-          <Route path="/movie/:id" exact={true}>
-            <Film />
-          </Route>
-          <Route path="/top" exact={true}>
-            <Top films={films} />
-          </Route>
-          <Route path="/recents" exact={true}>
-            <Recents films={films} />
-          </Route>
-        </Switch>
+        <ThemeContext.Provider value={theme}>
+          <Header
+            onSearch={searchByTitle}
+            onFocus={hideSlider}
+            onBlur={showSlider}
+            handleHomeClick={showHomeFilms}
+            handleTopClick={showTopFilms}
+            handleRecentClick={showRecentFilms}
+            handleThemeClick={changeTheme}
+          />
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home
+                films={films}
+                slider={slider}
+                page={page}
+                handleMoreClick={handleMoreClick}
+                handleLessClick={handleLessClick}
+                sliderHeight={sliderHeight}
+              />
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Film />
+            </Route>
+            <Route path="/top" exact={true}>
+              <Top films={films} />
+            </Route>
+            <Route path="/recents" exact={true}>
+              <Recents films={films} />
+            </Route>
+          </Switch>
+        </ThemeContext.Provider>
       </Router>
     </>
   );
